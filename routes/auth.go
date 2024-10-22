c840cdb1ca8ade1395b54d177ac1d05ef5fdcee6
erfan <mahdi.farahmandsafa@gmail.com>
2024-10-22 19:34:13 +0330

    add auth routes

---
+++ routes/auth.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/erfanfs10/Blog-Echo/handlers"
+	"github.com/labstack/echo/v4"
+)
+
+func AuthRoutes(g *echo.Group) {
+	g.POST("register/", handlers.Register)
+	g.POST("login/", handlers.Login)
+	g.POST("refresh/", handlers.RefreshToken)
+
+}
