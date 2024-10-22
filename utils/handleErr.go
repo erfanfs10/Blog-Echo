4cb74ec5caa4adeb3706e930e36d3a8f09bfc1f9
erfan <mahdi.farahmandsafa@gmail.com>
2024-10-22 22:59:34 +0330

    check error type

--- utils/handleErr.go
+++ utils/handleErr.go
@@ -1,8 +1,14 @@
 package utils
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
 
 func HandleError(c echo.Context, code int, err error, message string) error {
 	c.Set("err", err.Error())
-	return c.String(code, message)
+	if _, ok := err.(validator.ValidationErrors); ok {
+		return CustomValidationError(code, message, err, c)
+	}
+	return c.JSON(code, map[string]string{"message": message})
 }
