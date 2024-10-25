48a6f83d4deae1900ac6d450b4a4af8316287085
erfan <mahdi.farahmandsafa@gmail.com>
2024-10-25 13:08:50 +0330

    delete extra files

--- utils/claimsJWT.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/golang-jwt/jwt/v5"
-
-type JwtCustomClaims struct {
-	UserID    int    `json:"user_id"`
-	TokenType string `json:"token_type"`
-	jwt.RegisteredClaims
-}
