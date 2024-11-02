7492cf70571427ae6c2631d53f92d0aeac5fccbd
erfan <mahdi.farahmandsafa@gmail.com>
2024-11-02 19:05:41 +0330

    update handleErr func

--- utils/handleErr.go
+++ utils/handleErr.go
@@ -6,11 +6,16 @@ import (
 )
 
 func HandleError(c echo.Context, code int, err error, message string) error {
-	// set the error message into context
-	c.Set("err", err.Error())
-	// Check if the error is a validation error from `go-playground/validator`
-	if _, ok := err.(*validator.InvalidValidationError); ok {
-		return c.JSON(code, map[string]string{"message": message})
+	if err != nil {
+		// set the error message into context
+		c.Set("err", err.Error())
+		// Check if the error is a validation error from `go-playground/validator`
+		_, ok := err.(*validator.InvalidValidationError)
+		if ok {
+			return c.JSON(code, map[string]string{"message": message})
+		} else {
+			return CustomValidationError(code, message, err, c)
+		}
 	}
-	return CustomValidationError(code, message, err, c)
+	return c.JSON(code, map[string]string{"message": message})
 }
