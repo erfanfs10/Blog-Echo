1ded4b6ee41b90e5358242de7277f1ea8c3c1983
erfan <mahdi.farahmandsafa@gmail.com>
2024-12-01 17:33:37 +0330

    create Schema struct

--- db/schemas.go
+++ db/schemas.go
@@ -1,29 +1,33 @@
 package db
 
-var schemas = `
+type Schema struct {
+	Name  string
+	Query string
+}
 
-CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    is_active BOOLEAN DEFAULT TRUE,
-    is_admin BOOLEAN DEFAULT FALSE,
-    last_login DATETIME,
-    verification_code VARCHAR(10),
-    avatar VARCHAR(100),
-    password VARCHAR(255) NOT NULL,
-    created DATETIME DEFAULT NOW(),
-	updated DATETIME DEFAULT NOW() ON UPDATE NOW()
-);
-
-CREATE TABLE IF NOT EXISTS posts(
+var createTables = []Schema{
+	{Name: "Users", Query: `
+    CREATE TABLE IF NOT EXISTS users(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        is_active BOOLEAN DEFAULT TRUE,
+        is_admin BOOLEAN DEFAULT FALSE,
+        last_login DATETIME,
+        verification_code VARCHAR(10),
+        avatar VARCHAR(100),
+        password VARCHAR(255) NOT NULL,
+        created DATETIME DEFAULT NOW(),
+        updated DATETIME DEFAULT NOW() ON UPDATE NOW());
+    `},
+	{Name: "Posts", Query: `
+    CREATE TABLE IF NOT EXISTS posts(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title TEXT,
     body TEXT,
     user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     created DATETIME DEFAULT NOW(),
-	updated DATETIME DEFAULT NOW() ON UPDATE NOW()
-);
-
-`
+	updated DATETIME DEFAULT NOW() ON UPDATE NOW());
+    `},
+}
