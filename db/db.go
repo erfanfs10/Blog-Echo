9cd42d6d93d8a3c158fe2a7f524e879287fc92ab
erfan <mahdi.farahmandsafa@gmail.com>
2024-12-01 15:12:03 +0330

    get credentials from .env

--- db/db.go
+++ db/db.go
@@ -2,15 +2,25 @@ package db
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/erfanfs10/Blog-Echo/utils"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"log"
 )
 
 var DB *sqlx.DB
 
 func ConnectDb() {
-	db, err := sqlx.Connect("mysql", "root:12345@/blog?parseTime=true")
+
+	dbHost := utils.GetEnv("DB_HOST", "localhost")
+	dbUser := utils.GetEnv("DB_USER", "root")
+	dbPassword := utils.GetEnv("DB_PASSWORD", "12345")
+	dbName := utils.GetEnv("DB_NAME", "blog")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbName)
+
+	db, err := sqlx.Connect("mysql", dsn)
 	if err != nil {
 		fmt.Println("Can not connect to DB")
 		log.Fatalln(err)
