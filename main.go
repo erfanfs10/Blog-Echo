0261fae0c328414915f9a83408318f5f949d3a75
erfan <mahdi.farahmandsafa@gmail.com>
2024-12-04 17:34:51 +0330

    create root route - create template renderer

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"html/template"
+	"io"
+	"net/http"
+
 	"github.com/erfanfs10/Blog-Echo/db"
 	"github.com/erfanfs10/Blog-Echo/middlewares"
 	"github.com/erfanfs10/Blog-Echo/routes"
 	"github.com/erfanfs10/Blog-Echo/utils"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func init() {
@@ -16,17 +21,34 @@ func init() {
 	utils.CreateEmailChannel()
 }
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.Validator = utils.CreateCustomValidator()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", nil)
+	})
 
 	defer db.DB.Close()
 	defer close(utils.EmailChannel)
 
 	e.Use(middlewares.SeparateLogs())
 	e.Use(middlewares.CustomLogger())
-	// e.Use(middleware.Recover())
+	e.Use(middleware.Recover())
 
 	routes.AuthRoutes(e.Group("/auth/"))
 	routes.UserRoutes(e.Group("/user/"))
